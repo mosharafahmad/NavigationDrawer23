190ff6527bc756b3d7cd81a4e484614a85cafd6c
dent <Mosharafahmadofficial@gmail.com>
2026-05-05 16:35:38 +0600

    SliderAdapter

--- app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -3,21 +3,17 @@ package com.example.navigationdrawer;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -38,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    /*    EdgeToEdge.enable(this);  */
         setContentView(R.layout.activity_main);
 
         drawerLayout = findViewById(R.id.drawerLayout);
@@ -49,70 +44,50 @@ public class MainActivity extends AppCompatActivity {
         header_title = headerView.findViewById(R.id.header_title);
         imageheader = headerView.findViewById(R.id.imageheader);
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout, new Fragmentfirst());
-        fragmentTransaction.commit();
-
-
+        // Load the default fragment
+        loadFragment(new Fragmentfirst());
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                MainActivity.this, drawerLayout, materialToolbar, R.string.drawer_close, R.string.drawer_open);
 
-         MainActivity.this, drawerLayout,materialToolbar,R.string.drawer_close,R.string.drawer_open);
+        drawerLayout.addDrawerListener(toggle);
 
-      drawerLayout.addDrawerListener(toggle);
+        materialToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.profile)
+                    Toast.makeText(MainActivity.this, "Profile clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
 
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
 
-      materialToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-          @Override
-          public boolean onMenuItemClick(MenuItem item) {
+                if (itemId == R.id.mosharaf_dental) {
+                    Fragmentfirst.Web_url = "https://mosharaf-dental.netlify.app/";
+                    loadFragment(new Fragmentfirst());
+                } else if (itemId == R.id.Mostafiz_dental) {
+                    Fragmentfirst.Web_url = "https://mostafizursdental.com/";
+                    loadFragment(new Fragmentfirst());
+                } else if (itemId == R.id.second_fragment) {
+                    loadFragment(new FragmentSecond());
+                } else if (itemId == R.id.third_fragment) {
+                    loadFragment(new FragmentThird());
+                }
 
-             if (item.getItemId()==R.id.profile)
-             Toast.makeText(MainActivity.this, "Hellow", Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
 
-              return true;
-          }
-      });
-
-
-
-
-
-
-
-       navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-           @Override
-           public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-           if (menuItem.getItemId()==R.id.mosharaf_dental){
-
-              Fragmentfirst.Web_url = "https://mosharaf-dental.netlify.app/";
-               FragmentManager fm = getSupportFragmentManager();
-               FragmentTransaction fragmentTransaction = fm.beginTransaction();
-               fragmentTransaction.replace(R.id.frameLayout, new Fragmentfirst());
-               fragmentTransaction.commit();
-
-               drawerLayout.closeDrawer(GravityCompat.START);
-
-           } else if (menuItem.getItemId()==R.id.Mostafiz_dental) {
-
-               Fragmentfirst.Web_url = "https://mostafizursdental.com/";
-               FragmentManager fm = getSupportFragmentManager();
-               FragmentTransaction fragmentTransaction = fm.beginTransaction();
-               fragmentTransaction.replace(R.id.frameLayout, new Fragmentfirst());
-               fragmentTransaction.commit();
-               drawerLayout.closeDrawer(GravityCompat.START);
-
-           }
-               return true;
-           }
-       });
-
-
-      /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });    */
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
